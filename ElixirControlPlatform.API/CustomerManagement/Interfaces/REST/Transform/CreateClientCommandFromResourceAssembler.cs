0cc12a8691a2034b11374d3c4b0064c6fbe39060
petitavo <Gustavohuanca1020@gmail.com>
2024-10-31 21:39:09 -0500

    feat(customer-management): create client command from resource assembler added.

---
+++ ElixirControlPlatform.API/CustomerManagement/Interfaces/REST/Transform/CreateClientCommandFromResourceAssembler.cs
@@ -0,0 +1,22 @@
+﻿using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Commands;
+using ElixirControlPlatform.API.CustomerManagement.Interfaces.REST.Resources;
+
+namespace ElixirControlPlatform.API.CustomerManagement.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler to create a client command from a resource
+/// </summary>
+public static class CreateClientCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Converts a <see cref="CreateClientResource"/> to a <see cref="CreateClientsSourceCommand"/>
+    /// </summary>
+    /// <param name="resource">The <see cref="CreateClientResource"/> resource</param>
+    /// <returns>
+    /// A <see cref="CreateClientsSourceCommand"/> command
+    /// </returns>
+    public static CreateClientsSourceCommand ToCommandFromResource(CreateClientResource resource)
+    {
+        return new CreateClientsSourceCommand(resource.PersonName, resource.Dni, resource.Email, resource.BusinessName, resource.Phone, resource.Address, resource.Country, resource.City, resource.Ruc);
+    }
+}
