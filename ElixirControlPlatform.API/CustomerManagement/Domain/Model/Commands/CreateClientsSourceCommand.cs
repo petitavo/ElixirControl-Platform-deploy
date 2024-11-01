fc5c14cf0eea5e76ef1c7560b277df380f125116
petitavo <Gustavohuanca1020@gmail.com>
2024-10-31 21:39:50 -0500

    feat(customer-management): create clients source command added.

---
+++ ElixirControlPlatform.API/CustomerManagement/Domain/Model/Commands/CreateClientsSourceCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace ElixirControlPlatform.API.CustomerManagement.Domain.Model.Commands;
+
+public record CreateClientsSourceCommand(string PersonName, string Dni, string Email, string BusinessName, string Phone, string Address, string Country, string City, string Ruc);
