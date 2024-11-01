2233cbcbdd52997686f84461ad07eb00ce0de79c
petitavo <Gustavohuanca1020@gmail.com>
2024-10-31 21:35:01 -0500

    feat(customer-management): interface client query service added.

---
+++ ElixirControlPlatform.API/CustomerManagement/Domain/Services/IClientQueryService.cs
@@ -0,0 +1,33 @@
+﻿using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Aggregates;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Queries;
+
+namespace ElixirControlPlatform.API.CustomerManagement.Domain.Services;
+
+/// <summary>
+/// Client query service interface
+/// </summary>
+public interface IClientQueryService
+{
+    /// <summary>
+    /// Handle get client by id query
+    /// </summary>
+    /// <param name="query">GetClientsByIdQuery query</param>
+    /// <returns>
+    /// The client if successful otherwise null
+    /// </returns>
+    /// <see cref="GetClientByIdQuery"/>
+    Task<Client?> Handle(GetClientByIdQuery query);
+    
+    /// <summary>
+    /// Handle get all clients by dni query
+    /// </summary>
+    /// <param name="query">GetAllClientsByDniQuery query  </param>
+    /// <returns>
+    /// The list of clients if successful otherwise null
+    /// </returns>
+    /// see <see cref="GetAllClientsByDniQuery"/>
+    Task<IEnumerable<Client>> Handle(GetAllClientsByDniQuery query);
+    
+    
+    Task<IEnumerable<Client>> Handle(GetAllClientsQuery query);
+}
