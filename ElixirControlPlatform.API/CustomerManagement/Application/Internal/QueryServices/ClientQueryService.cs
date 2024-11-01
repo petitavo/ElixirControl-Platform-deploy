34ace70326ca5261191cb09d745f1663242212f8
petitavo <Gustavohuanca1020@gmail.com>
2024-10-31 21:37:25 -0500

    feat(customer-management): client query service added.

---
+++ ElixirControlPlatform.API/CustomerManagement/Application/Internal/QueryServices/ClientQueryService.cs
@@ -0,0 +1,28 @@
+﻿using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Aggregates;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Queries;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Repositories;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Services;
+namespace ElixirControlPlatform.API.CustomerManagement.Application.Internal.QueryServices;
+
+/// <summary>
+/// Query service for clients
+/// </summary>
+/// <param name="clientRepository">ClientRepository instance</param>
+public class ClientQueryService(IClientRepository clientRepository) : IClientQueryService
+{
+    /// <inheritdoc cref="IClientQueryService.Handle(GetClientByIdQuery)"/>
+    public async Task<Client?> Handle(GetClientByIdQuery query)
+    {
+        return await clientRepository.FindByIdAsync(query.Id);
+    }
+    
+    public async Task<IEnumerable<Client>> Handle(GetAllClientsByDniQuery query)
+    {
+        return await clientRepository.FindByDniAsync(query.Dni);
+    }
+    
+    public async Task<IEnumerable<Client>> Handle(GetAllClientsQuery query)
+    {
+        return await clientRepository.FindAllClientsAsync();
+    }
+}
