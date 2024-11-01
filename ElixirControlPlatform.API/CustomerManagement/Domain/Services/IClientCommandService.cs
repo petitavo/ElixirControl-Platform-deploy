f2dedd06e7bd280e4b0dd466f2c4f2659bc27b46
petitavo <Gustavohuanca1020@gmail.com>
2024-10-31 21:41:33 -0500

    feat(customer-management): interface client command service added.

---
+++ ElixirControlPlatform.API/CustomerManagement/Domain/Services/IClientCommandService.cs
@@ -0,0 +1,20 @@
+﻿using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Aggregates;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Commands;
+
+namespace ElixirControlPlatform.API.CustomerManagement.Domain.Services;
+
+/// <summary>
+/// Client command service interface
+/// </summary>
+public interface IClientCommandService
+{
+    /// <summary>
+    /// Handle create client command
+    /// </summary>
+    /// <param name="command">The CreateClientsSourceCommand</param>
+    /// <returns>
+    /// The created client if successful otherwise null
+    /// </returns>
+    /// <see cref="CreateClientsSourceCommand"/>
+    Task<Client?> Handle(CreateClientsSourceCommand command);
+}   
