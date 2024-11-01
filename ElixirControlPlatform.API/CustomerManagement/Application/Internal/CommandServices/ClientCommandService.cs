96a3929785a45af211e864b6e496e535fc410441
petitavo <Gustavohuanca1020@gmail.com>
2024-10-31 21:37:10 -0500

    feat(customer-management): client command service added.

---
+++ ElixirControlPlatform.API/CustomerManagement/Application/Internal/CommandServices/ClientCommandService.cs
@@ -0,0 +1,37 @@
+﻿using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Aggregates;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Model.Commands;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Repositories;
+using ElixirControlPlatform.API.CustomerManagement.Domain.Services;
+using ElixirControlPlatform.API.Shared.Domain.Repositories;
+
+namespace ElixirControlPlatform.API.CustomerManagement.Application.Internal.CommandServices;
+
+/// <summary>
+/// Client command service
+/// </summary>
+/// <param name="clientRepository">ClientRepository instance</param>
+/// <param name="unitOfWork">Unit of Work instance</param>
+/// 
+public class ClientCommandService(IClientRepository clientRepository, IUnitOfWOrk unitOfWork) : IClientCommandService
+{
+    /// <inheritdoc cref="IClientCommandService.Handle"/>
+    public async Task<Client?> Handle(CreateClientsSourceCommand command)
+    {
+        var client =
+            (await clientRepository.FindByDniAsync(command.Dni)).FirstOrDefault();
+        if (client != null)
+            throw new Exception("Client with DNI already exists");
+        client = new Client(command);
+        try 
+        {
+            await clientRepository.AddAsync(client);
+            await unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+
+        return client;
+    }
+}
